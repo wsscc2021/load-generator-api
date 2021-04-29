53d45c808a9cc57468f6dacc2b0fe709d6af6a8b
Donggeun Lee <piou987@gmail.com>
2021-04-29 21:26:01 +0900

    Init spring project

---
+++ src/main/java/cloud/skill53/sampleappfoo/SampleAppFooApplication.java
@@ -0,0 +1,13 @@
+package cloud.skill53.sampleappfoo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleAppFooApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SampleAppFooApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cloud/skill53/sampleappfoo/SampleAppFooApplicationTests.java
@@ -0,0 +1,13 @@
+package cloud.skill53.sampleappfoo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SampleAppFooApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
