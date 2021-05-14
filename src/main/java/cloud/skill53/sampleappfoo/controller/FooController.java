60384ad064e871490cf913e4ccd19ff6b3717f32
Donggeun Lee <piou987@gmail.com>
2021-05-14 11:23:56 +0900

    Add /error API for occur error

--- src/main/java/cloud/skill53/sampleappfoo/controller/FooController.java
+++ src/main/java/cloud/skill53/sampleappfoo/controller/FooController.java
@@ -54,4 +54,30 @@ public class FooController {
             return new ResponseEntity<>("Error Occur!", HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
+
+    @GetMapping("/error")
+    public ResponseEntity<?> getError(@RequestParam(defaultValue = "0") Integer code) {
+        switch(code) {
+            case 400:
+                return new ResponseEntity<>("400, Bad Request", HttpStatus.BAD_REQUEST);
+            case 401:
+                return new ResponseEntity<>("401, Unauthorized", HttpStatus.UNAUTHORIZED);
+            case 403:
+                return new ResponseEntity<>("403, Forbidden", HttpStatus.FORBIDDEN);
+            case 404:
+                return new ResponseEntity<>("404, Not Found", HttpStatus.NOT_FOUND);
+            case 500:
+                return new ResponseEntity<>("500, Internal Server Error", HttpStatus.INTERNAL_SERVER_ERROR);
+            case 501:
+                return new ResponseEntity<>("501, Not Implement", HttpStatus.NOT_IMPLEMENTED);
+            case 502:
+                return new ResponseEntity<>("502, Bad Gateway", HttpStatus.BAD_GATEWAY);
+            case 503:
+                return new ResponseEntity<>("503, Unavailable", HttpStatus.SERVICE_UNAVAILABLE);
+            case 504:
+                return new ResponseEntity<>("504, Gateway Timeout", HttpStatus.GATEWAY_TIMEOUT);
+            default:
+                return new ResponseEntity<>("200, This code is not found", HttpStatus.OK);
+        }
+    }
 }
